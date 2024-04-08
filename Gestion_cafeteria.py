3754433b66553a480ccecd345c9023cb985bdcd8
ChrisJimenez29 <A01736302@tec.mx>
2024-04-07 18:58:45 -0600

    Documentacion

--- Gestion_cafeteria.py
+++ Gestion_cafeteria.py
@@ -2,13 +2,13 @@
 #Pruebas Sistema de Gestión de Cafetería
 
 def agregar_bebida(entrada):
-    # Eliminar espacios en blanco de la entrada
+    # Elimina espacios en blanco de la entrada
     entrada = entrada.replace(" ", "")
     
-    # Dividir la entrada por la coma para separar el nombre de los tamaños
+    # Divide la entrada por la coma para separar el nombre de los tamaños
     partes = entrada.split(",")
     
-    # Verificar que haya al menos dos partes (nombre y al menos un tamaño)
+    # Verifica que haya al menos dos partes (nombre y al menos un tamaño)
     if len(partes) < 2:
         return False
     
@@ -17,21 +17,21 @@ def agregar_bebida(entrada):
     if not nombre_articulo.isalpha() or not (2 <= len(nombre_articulo) <= 15):
         return False
     
-    # Extraer y validar los tamaños
+    # Extrae y valida los tamaños
     tamanos = partes[1:]
     
-    # Verificar que haya de 1 a 5 tamaños
+    # Verifica que haya de 1 a 5 tamaños
     if not (1 <= len(tamanos) <= 5):
         return False
     
-    # Convertir tamaños a enteros y verificar que estén en el rango de 1 a 48 y en orden ascendente
+    # Convierte tamaños a enteros y verifica que estén en el rango de 1 a 48 y en orden ascendente
     tamanos_enteros = []
     ultimo_tamano = 0
     for tamano in tamanos:
         if not tamano.isdigit() or not (1 <= int(tamano) <= 48):
             return False
         tamano_entero = int(tamano)
-        if tamano_entero <= ultimo_tamano: # Verificar orden ascendente
+        if tamano_entero <= ultimo_tamano: # Verifica orden ascendente
             return False
         ultimo_tamano = tamano_entero
         tamanos_enteros.append(tamano_entero)
